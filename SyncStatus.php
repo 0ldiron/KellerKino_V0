8ecdc058c67ea070519438f1dd5658278cf0a552
0ldiron <olafbarth@nexgo.de>
2015-03-31 18:41:29 +0200

    31.03.15 Kein Sync bei fehlender IMDBID

--- SyncStatus.php
+++ SyncStatus.php
@@ -8,11 +8,14 @@ $res2 = $db2->query('SELECT c09 FROM movie');
 $updates = '';
 while($row2 = $res2->fetchArray(SQLITE3_ASSOC))
 {
-	$db1->exec('UPDATE movies SET status=2 WHERE status<>2 AND imdb_id="'.$row2['c09'].'"');
-	if ($db1->changes())
+	if (!empty($row2['c09']))
 	{
-		if (empty($updates)) $updates = '"'.$row2['c09'].'"';
-		else $updates .= ',"'.$row2['c09'].'"';
+		$db1->exec('UPDATE movies SET status=2 WHERE status<>2 AND imdb_id="'.$row2['c09'].'"');
+		if ($db1->changes())
+		{
+			if (empty($updates)) $updates = '"'.$row2['c09'].'"';
+			else $updates .= ',"'.$row2['c09'].'"';
+		}
 	}
 }
 $db1->close();
